0986b0a25dcd70c329c8e1b7c8ea9140e4cec875
vishal-Tx <vishalguptaa2z111@gmail.com>
2022-12-12 03:49:20 +0530

    api implementation done

--- Controllers/openaiRoutes.js
+++ Controllers/openaiRoutes.js
@@ -1,1 +1,24 @@
-export const generateImage = async (req, res) => {};
+import { Configuration, OpenAIApi } from "openai";
+
+// const response = await openai.listEngines();
+
+export const generateImage = async (req, res) => {
+  const configuration = new Configuration({
+    organization: process.env.OPENAI_ORGANIZATION_KEY,
+
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+  console.log(req.body);
+  const { desc, selected: size } = req.body;
+  const imageSize =
+    size === "small" ? "256x256" : size === "medium" ? "512x512" : "1024x1024";
+
+  const response = await openai.createImage({
+    prompt: desc,
+    n: 1,
+    size: imageSize,
+  });
+  const image_url = response.data.data[0].url;
+  console.log(image_url);
+};
--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,20 +1,37 @@
-import React from "react";
+import React, { useRef } from "react";
+import axios from "axios";
 
 const Form = () => {
+  const url = "http://localhost:5000/openai";
+  const inputref = useRef(null);
   const handleSubmit = async (e) => {
     e.preventDefault();
+    let selected = document.getElementById("size").value;
+    console.log(selected);
+    alert(inputref.current.value + selected);
+    const desc = inputref.current.value;
+    const response = await axios.post(`${url}/generateimage`, {
+      desc,
+      selected,
+    });
   };
   return (
     <div className="flex justify-center items-center h-56 bg-red-400">
       <div className="flex flex-col items-center">
         <h1 className="text-4xl font-bold">Describe an Image.</h1>
         <form onSubmit={handleSubmit}>
-          <input className=" w-96 h-10 font-semibold text-xl my-4"></input>
-          <select name="size" id="size" className="w-96 h-10 block  mb-4">
+          <input
+            className=" w-96 h-10 font-semibold text-xl my-4"
+            ref={inputref}
+          ></input>
+          <select
+            name="size"
+            id="size"
+            className="w-96 h-10 block  mb-4"
+            defaultValue="medium"
+          >
             <option value="small">Small</option>
-            <option value="medium" selected>
-              Medium
-            </option>
+            <option value="medium">Medium</option>
             <option value="large">Large</option>
           </select>
           <button
